9370531a9dfdccc1945e06eb86d44f506d09433f
LewieJ08 <lewiej08@gmail.com>
2025-11-22 23:37:33 +0000

    Configure env vars

---
+++ src/config/env.ts
@@ -0,0 +1,5 @@
+require('dotenv').config();
+
+export const REPO = process.env.REPO || undefined;
+export const OWNER = process.env.OWNER || undefined;
+export const TOKEN = process.env.TOKEN || undefined;
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
+const { REPO, OWNER, TOKEN, } = require('./config/env');
+
 function main(): void {
-    console.log('PR-CLI')
+    console.log('PR-CLI');
 }
 
-function dev(): void {
-    console.log('PR-CLI DEVMODE');
-}
-
-if (require.main == module) {
-    dev();
-} else {
-    main();
-}
+main()
