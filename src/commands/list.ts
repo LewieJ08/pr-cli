c83120138c333ee573f0b7a6c77185726e41994b
LewieJ08 <lewiej08@gmail.com>
2025-12-15 22:58:07 +0000

    Update list command using GithubService listPullRequests method

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -1,7 +1,28 @@
-import { listPullRequests } from "../services/githubService";
+import { TOKEN } from "../config/env";
+import { resolveGitContext } from "../utils/gitContext";
+import { GithubService } from "../services/githubService";
+import { NoGitRepoError, InvalidRemoteUrlError } from "../utils/gitUtils";
 
-async function list(): Promise<void> {
-    const pullRequests = await listPullRequests();
+async function listCommand(): Promise<void> {
+    try {
+        const { owner, repo } = resolveGitContext();
+        const github = new GithubService({token: TOKEN, owner: owner, repo: repo});
+
+        const pullRequests = await github.listPullRequests();
+        console.log(pullRequests);
+    } catch (error) {
+        if (error instanceof NoGitRepoError) {
+            console.log(error.message);
+            process.exit(1);
+        }
+
+        if (error instanceof InvalidRemoteUrlError) {
+            console.log(error.message);
+            process.exit(1);
+        }
+        
+        throw error;
+    }
 }
 
-export default list;
+export default listCommand;
