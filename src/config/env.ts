2fcb142b4b72c2f74c8d7acda6b2d43301471f7f
LewieJ08 <lewiej08@gmail.com>
2025-12-15 18:55:42 +0000

    Add env var validation

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,16 @@
 import { configDotenv } from "dotenv";
 configDotenv({quiet: true});
 
-// GitHub api token
-export const TOKEN = process.env.TOKEN || undefined;
+// fetch env vars
+function fetchEnvVar(name: string): string {
+    const value = process.env[name];
+
+    if (!value) {
+        console.log(`Missing required environment variable: ${name}`);
+        process.exit(1)
+    }
+
+    return value;
+}
+
+export const TOKEN = fetchEnvVar('TOKEN');
