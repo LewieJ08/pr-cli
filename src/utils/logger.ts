d10c28ff730a79f7be95980145d602005179c8c7
LewieJ08 <lewiej08@gmail.com>
2025-12-18 23:40:34 +0000

    Add logWarn util func

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -11,3 +11,7 @@ export function logError(message: string): void {
 export function logInfo(message: string): void {
     console.log(chalk.blue(`\n${message}`));
 }
+
+export function logWarn(message: string): void {
+    console.log(chalk.yellow(`\n${message}`));
+}
