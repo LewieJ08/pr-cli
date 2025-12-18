ad55635dab96824c8f6112a7d14adcf914707a8e
LewieJ08 <lewiej08@gmail.com>
2025-12-18 16:18:15 +0000

    Add logger utils

---
+++ src/utils/logger.ts
@@ -0,0 +1,9 @@
+import chalk from "chalk";
+
+export function logSuccess(message: string): void {
+    console.log(chalk.green(message));
+}
+
+export function logError(message: string): void {
+    console.log(chalk.red(message));
+}
