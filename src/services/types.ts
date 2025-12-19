e6b1e54d16121f9a6946616694420f25004fc526
LewieJ08 <lewiej08@gmail.com>
2025-12-19 00:00:44 +0000

    Add PullRequest type for GithubServer class

--- src/services/types.ts
+++ src/services/types.ts
@@ -10,3 +10,18 @@ export interface Repository {
     html_url: string;
     default_branch: string;
 }
+
+export interface PullRequest {
+    id: number;
+    html_url: string;
+    number: number;
+    title: string;
+    user: User;
+    head: {
+        ref: string;
+    }
+    base: {
+        ref: string;
+    }
+
+}
