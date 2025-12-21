e857ce87411d94fe604bba051fb8e7f11e9e4733
LewieJ08 <lewiej08@gmail.com>
2025-12-21 21:33:42 +0000

    Add colors export vars

---
+++ src/utils/color.ts
@@ -0,0 +1,8 @@
+import chalk from "chalk";
+
+export const success = chalk.green;
+export const error = chalk.red;
+export const info = chalk.blue;
+export const warn = chalk.yellowBright;
+export const bold = chalk.bold;
+export const dim = chalk.dim;
