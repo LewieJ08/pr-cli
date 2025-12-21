8e1e08eab9362749a5182b6bd26b1fe772adccf4
LewieJ08 <lewiej08@gmail.com>
2025-12-21 21:39:47 +0000

    Refactor logger.ts utils

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,49 +1,18 @@
-import chalk, { ChalkInstance } from "chalk";
+import * as c from './color.js' 
 
-interface LogOptions {
-    bold?: boolean;
-    dim?: boolean;
-    italic?: boolean;
-    underline?: boolean;
-    inverse?: boolean;
+// Helper log functions
+export function logSuccess(message: string): void {
+    console.log(c.success(message));
 }
 
-// Applies formatting options to base ChalkInstance
-function applyLogOptions(base: ChalkInstance, options?: LogOptions): ChalkInstance {
-    let style = base;
-
-    if (options?.bold) style = style.bold;
-    if (options?.dim) style = style.dim;
-    if (options?.italic) style = style.italic;
-    if (options?.underline) style = style.underline;
-    if (options?.inverse) style = style.inverse;
-
-    return style;
+export function logError(message: string): void {
+    console.log(c.error(message));
 }
 
-// Main log function
-function log( message: string, base: ChalkInstance, options?: LogOptions ): void {
-    const chalkInstance = applyLogOptions(base, options);
-    console.log(chalkInstance(`${message}`));
+export function logInfo(message: string, ): void {
+    console.log(c.info(message));
 }
 
-// Public log functions
-export function logSuccess(message: string, options?: LogOptions): void {
-    log(message, chalk.green, options);
-}
-
-export function logError(message: string, options?: LogOptions): void {
-    log(message, chalk.red, options);
-}
-
-export function logInfo(message: string, options?: LogOptions): void {
-    log(message, chalk.blue, options);
-}
-
-export function logWarn(message: string, options?: LogOptions): void {
-    log(message, chalk.yellowBright, options);
-}
-
-export function logText(message: string, options?: LogOptions): void {
-    log(message, chalk, options);
+export function logWarn(message: string, ): void {
+    console.log(c.warn(message));
 }
