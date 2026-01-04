e76913e17b8bdbbbd8ba8f6de0e6f4b381bb21b7
LewieJ08 <lewiej08@gmail.com>
2026-01-04 20:42:38 +0000

    Add github types file

---
+++ src/services/github.types.ts
@@ -0,0 +1,52 @@
+export interface User {
+    id: number;
+    login: string; // Username
+    email: string;
+}
+
+export interface Repository {
+    id: number;
+    name: string;
+    full_name: string;
+    html_url: string;
+    default_branch: string;
+}
+
+export interface Commit {
+    sha: number;
+    commit: {
+        author: {
+            name: string;
+            email: string;
+            date: string;
+        }
+        message: string;
+    }
+}
+
+export interface File {
+    sha: number;
+    filename: string;
+    additions: number;
+    deletions: number;
+    changes: number;
+    patch: string;
+}
+
+export interface PullRequest {
+    id: number;
+    html_url: string;
+    number: number;
+    state: 'open' | 'closed';
+    title: string;
+    user: User;
+    body?: string;
+    created_at: string;
+    head: {
+        ref: string;
+    }
+    base: {
+        ref: string;
+    }
+    merged: boolean;
+}
