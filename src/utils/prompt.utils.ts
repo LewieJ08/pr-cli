f769e8f6afe053ce32f81c6877807acfad2625fc
LewieJ08 <lewiej08@gmail.com>
2026-01-16 22:50:05 +0000

    Update prompt util to return null upon blank input

--- src/utils/prompt.utils.ts
+++ src/utils/prompt.utils.ts
@@ -1,6 +1,6 @@
 import readline from "node:readline";
 
-export function prompt(query: string): Promise<string> {
+export function prompt(query: string): Promise<string | null> {
     return new Promise(resolve => {
         const rl = readline.createInterface({
             input: process.stdin,
@@ -9,6 +9,12 @@ export function prompt(query: string): Promise<string> {
 
         rl.question(query, answer => {
             rl.close();
+            const trimedAnswer = answer.trim();
+            
+            if (trimedAnswer === '') {
+                resolve(null)
+            }
+
             resolve(answer.trim())
         });
     });
